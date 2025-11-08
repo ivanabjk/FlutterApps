fef5c2e31ab7847159634be279c063b3e94a671e
ivanabjk <ivana.bojkovska.002@gmail.com>
2025-11-09 00:44:41 +0100

    Added Ecommerce App

---
+++ ecommerce_app/lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:ecommerce_app/components/bottom_nav_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(),
+    );
+  }
+}
