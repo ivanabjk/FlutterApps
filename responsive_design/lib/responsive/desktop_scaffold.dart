ffe86c7243de867c41c4d7c7f9cb45a213400b1a
ivanabjk <ivana.bojkovska.002@gmail.com>
2025-11-30 02:34:36 +0100

    Initial commit

---
+++ responsive_design/lib/responsive/desktop_scaffold.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_design/constants.dart';
+
+import '../util/my_box.dart';
+import '../util/my_tile.dart';
+
+class DesktopScaffold extends StatefulWidget {
+  const DesktopScaffold({super.key});
+
+  @override
+  State<DesktopScaffold> createState() => _DesktopScaffoldState();
+}
+
+class _DesktopScaffoldState extends State<DesktopScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: myDefaultBackground,
+      appBar: myAppbar,
+      body: Row(
+        children: [
+          // open drawer
+          myDrawer,
+          // rest of body
+
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                // 4 boxes on the top
+                AspectRatio(
+                  aspectRatio: 4,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: GridView.builder(
+                      itemCount: 4,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4),
+                      itemBuilder: (context, index) {
+                        return MyBox();
+                      },
+                    ),
+                  ),
+                ),
+
+                // tiles below it
+
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return MyTile();
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.pink,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
